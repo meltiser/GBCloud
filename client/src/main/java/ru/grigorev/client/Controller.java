e61f8f0c5d96226a761f578e354ec858b4c03106
meltiser <dmitriy-95@yandex.ru>
2018-11-06 13:25:15 +0300

    added GUI

--- client/src/main/java/ru/grigorev/client/Controller.java
+++ client/src/main/java/ru/grigorev/client/Controller.java
@@ -1,14 +1,27 @@
 package ru.grigorev.client;
 
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.TransferMode;
 import javafx.stage.FileChooser;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
+import java.awt.*;
 import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
@@ -31,6 +44,8 @@ public class Controller implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
         clientListView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
         serverListView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+        initializeDragAndDropClientListView();
+        openAuth();
     }
 
     public void setPrimaryStage(Stage primaryStage) {
@@ -47,7 +62,7 @@ public class Controller implements Initializable {
         clientListView.getSelectionModel().clearSelection();
     }
 
-    public void showAbout(ActionEvent actionEvent) {
+    public void showHelp(ActionEvent actionEvent) {
         Alert alert = new Alert(Alert.AlertType.INFORMATION, "cool huh?", ButtonType.NO);
         alert.show();
     }
@@ -100,5 +115,59 @@ public class Controller implements Initializable {
     public void clearSelection() { //??????????????
         /*if (clientListView.getSelectionModel().getSelectedItem() != null)
             clientListView.getSelectionModel().clearSelection();*/
+    }
+
+
+    public void exit(ActionEvent actionEvent) {
+        Platform.exit();
+    }
+
+    public void openAuth() {
+        try {
+            Stage stage = new Stage();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Authorization.fxml"));
+            Parent root = loader.load();
+            Authorization auth = loader.getController();
+            //openAuth.id = 100;
+            stage.setTitle("Autorization");
+            stage.setScene(new Scene(root, 400, 200));
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.showAndWait();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void initializeDragAndDropClientListView() {
+        clientListView.setOnDragOver(event -> {
+            if (event.getGestureSource() != clientListView && event.getDragboard().hasFiles()) {
+                event.acceptTransferModes(TransferMode.COPY_OR_MOVE);
+            }
+            event.consume();
+        });
+
+        clientListView.setOnDragDropped(event -> {
+            Dragboard db = event.getDragboard();
+            boolean success = false;
+            if (db.hasFiles()) {
+                //clientListView.setText("");
+                for (File file : db.getFiles()) {
+                    clientList.add(file.getName() + " ");
+                }
+                success = true;
+                clientListView.setItems(clientList);
+            }
+            event.setDropCompleted(success);
+            event.consume();
+        });
+    }
+
+    public void openLink(ActionEvent actionEvent) {
+        Desktop desktop = Desktop.getDesktop();
+        try {
+            desktop.browse(new URI("https://github.com/meltiser/GBCloud"));
+        } catch (IOException | URISyntaxException e) {
+            e.printStackTrace();
+        }
     }
 }
